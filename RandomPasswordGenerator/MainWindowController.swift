de52915d2dd915f5372f4f540529c6fb49f95db2
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-06-24 11:34:19 -0400

    initial commit with UI that has been hooked up.

---
+++ RandomPasswordGenerator/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  RandomPasswordGenerator
+//
+//  Created by MLS Discovery on 6/23/16.
+//  Copyright © 2016 SoftwareSoFast. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
